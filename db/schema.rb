a3baf4b969427d96b91f4c968da72531795ab8af
tech-maru <tech@shihousyoshi.cc>
2019-12-11 07:15:41 +0000

    Add_colum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191207132023) do
+ActiveRecord::Schema.define(version: 20191211065239) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -31,9 +31,12 @@ ActiveRecord::Schema.define(version: 20191207132023) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
-    t.string "department"
     t.datetime "basic_time", default: "2019-12-10 23:00:00"
     t.datetime "work_time", default: "2019-12-10 22:30:00"
+    t.string "affiliation"
+    t.integer "employee_number"
+    t.string "uid"
+    t.boolean "superior"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
